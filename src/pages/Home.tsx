31ae3069d79f6dbce11bc75c6961d48cd0f651a9
Saalim-962 <Saalimshaikh078@gmail.com>
2026-04-09 11:13:53 +0530

    fix: home landing  UI chnges

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -495,17 +495,19 @@ export default function Home() {
           </Suspense>
         </LazySection>
 
-        <LazySection minHeight={520}>
-          <Suspense fallback={<SectionSkeleton height={520} />}>
-            <Addvertise content={advertiseContent} />
-          </Suspense>
-        </LazySection>
-
+     
         <LazySection minHeight={560}>
           <Suspense fallback={<SectionSkeleton height={560} />}>
             <HomeTrending content={trendingContent} />
           </Suspense>
         </LazySection>
+
+   <LazySection minHeight={750}>
+          <Suspense fallback={<SectionSkeleton height={520} />}>
+            <Addvertise content={advertiseContent} />
+          </Suspense>
+        </LazySection>
+
 
         <LazySection minHeight={180}>
           <Suspense fallback={<SectionSkeleton height={180} />}>
